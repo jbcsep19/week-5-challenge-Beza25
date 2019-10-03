54f40b858e81f230175e44195e2e3300600b24fe
Beza25 <sirak.bezawit@gmail.com>
2019-10-03 17:00:58 -0400

    added home and job form

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,16 +3,32 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @Autowired
     JobsRepository jobsRepository;
-
     @RequestMapping("/")
+    public String homePage(){
+        return "home";
+    }
+    @RequestMapping("/viewJobs")
     public String jobsList(Model model){
         model.addAttribute("jobs",jobsRepository.findAll());
+        return "jobForm";
+    }
+
+    @GetMapping("/postJob")
+    public String postJob(Model model){
+        model.addAttribute("job", new Jobs());
+        return "jobForm";
+    }
+    @GetMapping("/viewJob")
+    public String viewJob(Model model){
+        model.addAttribute("job", new Jobs());
         return "list";
     }
 }
